0a8d8849abe514962f3ca61a0db75c6b464e2b68
subahsh <subhashsharma931910@gmail.com>
2026-07-25 10:33:38 +0530

    dataTypes

---
+++ JavaScript/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strick"; // Treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "subhash" //string
+let age = 21
+let isLoggedIn = false;
+
+//number => 2 to 53
+//bigint
+//string => ""
+// boolean => true/false
+//null = standalone value
+// undefine =>
+// symbol => unique 
+
+
+//object 
+console.log(typeof undefined) //undefine
+console.log(typeof null); //object
+
