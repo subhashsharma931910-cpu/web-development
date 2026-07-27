d48a6372b5b81a6dd84c5c6aecf786ed088cf385
subhash <subhashsharma931910@gmail.com>
2026-07-27 05:06:27 +0000

    03_conversionOperation.js

---
+++ JavaScript/01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33abc";
+
+// const {score} = req.body;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0;
+
+let isLoggedIn  = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// true = 1; false = 0;
+// "" = false
+// "subhash" = true
+
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
